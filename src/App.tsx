180a30201e777ec891046448a8e5c1449ac08911
dain <57316771+DainPark-web@users.noreply.github.com>
2023-11-13 10:00:02 +0100

    update

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-    return (
-       <>
-         <div>dain</div>
-       </>
-    )
-}
-
-export default App;
---
+++ src/components/button/button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Button = () => {
+    return (
+        <div>
+            Dina
+        </div>
+    )
+}
+
+export default Button;
