8f01b50893c6684a5c0cca46607b5820b6a7ce84
dain <57316771+DainPark-web@users.noreply.github.com>
2023-11-14 02:11:27 +0100

    update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import { dts } from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
@@ -27,7 +27,7 @@ export default [
     ],
   },
   {
-    input: "dist/esm/index.d.ts",
+    input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
